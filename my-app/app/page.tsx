92569af45fa58112c6aa1451340cc9cab8c8ade0
hakimcodes-algos <156512444+hakimcodes-algos@users.noreply.github.com>
2026-01-11 01:57:27 +0300

    Update page.tsx

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -10,7 +10,6 @@ export default function HomePage() {
       <HeroSection />
       <ServicesSection />
       <AboutSection />
-      <BlogInsightsSection />
-   </RootLayout>
+    </RootLayout>
   );
 }
